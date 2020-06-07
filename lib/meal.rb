3f64d3c6c5d6f4eaef29242b78c26909aa4f63ff
alexpac27 <alexpac27@gmail.com>
2020-06-07 01:57:15 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,36 @@
-class Meal
+require_relative './customer.rb'
+# require_relative './lib/meal.rb'
+require_relative './waiter.rb'
+require 'pry'
 
-end
+class Meal
+ 
+    attr_accessor :waiter, :customer, :total, :tip
+   
+    @@all = []
+   
+    def initialize(waiter, customer, total, tip=0)
+      @waiter = waiter
+      @customer = customer
+      @total = total
+      @tip = tip
+      @@all << self
+    end
+   
+    def self.all
+      @@all
+    end
+  end
+  
+jason = Waiter.new("Jason", 4)
+sarah = Waiter.new("Sarah",1)
+
+lisa = Customer.new("Lisa", 24)
+tim = Customer.new("Tim", 35)
+terrance = Customer.new("Terrance", 27)
+
+terrance.new_meal(jason, 50, 5)
+lisa.new_meal(jason, 40, 15)
+tim.new_meal(sarah, 45, 2)
+lisa.new_meal(sarah, 35, 6)
+  binding.pry
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,64 @@
 class Waiter
+ 
+    attr_accessor :name, :yrs_experience
+    attr_reader :customer, :total, :tip
+   
+    @@all = []
+   
+    def initialize(name, yrs_experience)
+      @name = name
+      @yrs_experience = yrs_experience
+      @@all << self
+    end
+   
+    def self.all
+      @@all
+    end
 
-end
+    def new_meal(customer, total, tip=0)
+      Meal.new(self, customer, total, tip)
+    end
+  
+    def meals
+      Meal.all.select do |meal|
+        meal.waiter == self #checking for waiter now
+      end
+    end
+
+    def best_tipper
+      best_tipped_meal = meals.max do |meal_a, meal_b|
+        meal_a.tip <=> meal_b.tip
+      end
+     
+      best_tipped_meal.customer
+    end
+   
+    def most_frequent_customer
+      all_customers = self.meals.map {|meal| meal.customer.name}
+      all_customers.max_by{|cus| all_customers.count(cus)}
+    end
+
+    def worst_meal
+      all_tips = self.meals.map {|meal| meal.tip}
+      self.meals.find {|meal| meal.tip == all_tips.min}
+    end
+
+    def self.average_tips
+     
+      all_yrs_exp = Waiter.all.map{|waiter| waiter.yrs_experience}
+
+      waiter_most_exp = Waiter.all.find {|waiter| waiter.yrs_experience == all_yrs_exp.max}
+      most_exp_tips_array = waiter_most_exp.meals.map{|meal| meal.tip}
+      most_avg_tips = most_exp_tips_array.inject{ |sum, el| sum + el }.to_f / most_exp_tips_array.size
+      
+      waiter_least_exp = Waiter.all.find {|waiter| waiter.yrs_experience == all_yrs_exp.min}
+      least_exp_tips_array = waiter_least_exp.meals.map{|meal| meal.tip}
+      least_avg_tips = least_exp_tips_array.inject{ |sum, el| sum + el }.to_f / least_exp_tips_array.size
+      
+       hash = {most_exp: most_avg_tips, least_exp: least_avg_tips}
+      p hash
+    end
+
+  end
+
+  
